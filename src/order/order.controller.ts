2898d3c1b3aeae9209e2b8cc9bc629f1219ef5dc
kiennvm <thanhkiennvm@gmail.com>
2023-06-07 21:43:04 +0700

    update order entity

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,33 +1,26 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-
-  UseGuards,
-} from '@nestjs/common'
-import { OrderService } from './order.service'
-import { CreateOrderDto } from './dto/create-order.dto'
-import { GetOrderDto } from './dto/get-order.dto'
-import { UpdateStatusDto } from './dto/update-status.dto'
-import { JwtAuthGuard } from 'src/auth/guard/jwt.guard'
+import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
+import { OrderService } from './order.service';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { GetOrderDto } from './dto/get-order.dto';
+import { UpdateStatusDto } from './dto/update-status.dto';
+import { JwtAuthGuard } from 'src/auth/guard/jwt.guard';
 
 @Controller('order')
 @UseGuards(JwtAuthGuard)
 export class OrderController {
-  constructor (private readonly orderService: OrderService) {}
+  constructor(private readonly orderService: OrderService) {}
 
   @Post('create')
-  async create (@Body() createOrderDto: CreateOrderDto) {
-    console.log(createOrderDto)
-    return await this.orderService.create(createOrderDto)
+  async create(@Body() createOrderDto: CreateOrderDto) {
+    console.log(createOrderDto);
+    return await this.orderService.create(createOrderDto);
   }
-  @Post('')
-  async getOrder (@Body() dto: GetOrderDto) {
-    return await this.orderService.getOrders(dto)
+  @Post()
+  async getOrder(@Body() dto: GetOrderDto) {
+    return await this.orderService.getOrders(dto);
   }
   @Post('update-status')
-  async updateStatus (@Body() dto: UpdateStatusDto) {
-    return await this.orderService.updateStatus(dto)
+  async updateStatus(@Body() dto: UpdateStatusDto) {
+    return await this.orderService.updateStatus(dto);
   }
 }
